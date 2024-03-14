cec49d57c1f9f70f570a33411067726df3dfdd92
HBrough-6 <130100349+HBrough-6@users.noreply.github.com>
2024-03-14 09:53:37 -0700

    New Game/Load Game Screen Creation
    Created basic templates for the load game and new game screens. Also started the base of the menu navigation system.

---
+++ GhostsGhoulsGoblins/GhostsGhoulsGoblins/Assets/Scripts/UI Scripts/ScreenInUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Brough, Heath
+// 3/14/24
+// template script for menu screens
+
+public class ScreenInUI : MonoBehaviour
+{
+    // holds the screen to return to when pressing esc/going 
+    private GameObject previousScreen;
+    // holds references to the next possible screens that the player could select
+    private GameObject[] NextScreens;
+
+    public void GoPreviousScene()
+    {
+
+    }
+
+    public void GoNextScene()
+    {
+
+    }
+}
