23e36f78db52a3715a3077ef822f9fad5b8c29b8
HBrough-6 <130100349+HBrough-6@users.noreply.github.com>
2024-04-02 09:34:12 -0700

    UIManager Complete
    UIManager has functions to update health, gold count, and the percentage of the dive charge

--- GhostsGhoulsGoblins/GhostsGhoulsGoblins/Assets/Scripts/UI Scripts/UIManager.cs
+++ GhostsGhoulsGoblins/GhostsGhoulsGoblins/Assets/Scripts/UI Scripts/UIManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
@@ -15,6 +17,8 @@ public class UIManager : MonoBehaviour
     // HUD element references
     [SerializeField] private GameObject _HUDRef;
     [SerializeField] private GameObject HealthBarRef;
+    [SerializeField] private GameObject DiveStrengthBar;
+    [SerializeField] private GameObject MoneyTrackerRef;
 
     // UI Element arrays
     [SerializeField] private GameObject[] _HUDElements = new GameObject[5];
@@ -26,7 +30,7 @@ public class UIManager : MonoBehaviour
 
         _HUDRef = GameObject.FindGameObjectWithTag("HUD");
 
-        int index = 0;
+        /*int index = 0;
         GameObject currentChild;
         // finds all of the children of the HUD and puts them in a list
         while ((currentChild = _HUDRef.transform.GetChild(index).gameObject) != null)
@@ -34,7 +38,11 @@ public class UIManager : MonoBehaviour
             _HUDElements[index] = currentChild;
             currentChild = null;
             index++;
-        }
+        }*/
+
+        HealthBarRef = _HUDRef.transform.GetChild(1).gameObject;
+        DiveStrengthBar = _HUDRef.transform.GetChild(2).gameObject;
+        MoneyTrackerRef = _HUDRef.transform.GetChild(3).gameObject;
     }
 
     
@@ -74,21 +82,41 @@ public class UIManager : MonoBehaviour
         Time.timeScale = 1;
     }
 
-    public void UpdateHealth(int newHealthTotal)
+    /// <summary>
+    /// updates the health in the UI
+    /// </summary>
+    /// <param name="newHealthTotal">the new value that health is at</param>
+    /// <param name="maxHealth">the maximum health the player can have</param>
+    public void UpdateHealth(int newHealthTotal, int maxHealth)
     {
+        float healthPercent = newHealthTotal / maxHealth;
         // update the health in the UI
-        
-
+        HealthBarRef.transform.GetChild(1).GetComponent<TMP_Text>().text = newHealthTotal.ToString();
+        HealthBarRef.transform.GetChild(0).localScale = new Vector3(healthPercent , 1, 1);
     }
 
     public void UpdateGold(int newGoldTotal)
     {
         // update the gold in the UI
+        MoneyTrackerRef.transform.GetChild(0).GetComponent<TMP_Text>().text = newGoldTotal.ToString();
     }
 
-    public void UpdateDiveCharge()
+    /// <summary>
+    /// updates the charge percentage in the UI
+    /// </summary>
+    /// <param name="percentComplete">number from 0-1 indicating the completeness of the charge 0 uncharged, 1 fully charged</param>
+    public void UpdateDiveCharge(float percentComplete)
     {
+        if (percentComplete == 0)
+        {
+            DiveStrengthBar.GetComponent<Image>().color = Color.grey;
+            DiveStrengthBar.GetComponent<RectTransform>().transform.localScale = new Vector3(0.1f, 1, 1);
+        }
+        else
+        {
+            DiveStrengthBar.GetComponent<Image>().color = Color.blue;
+            DiveStrengthBar.GetComponent<RectTransform>().transform.localScale = new Vector3(0.1f + 0.9f * percentComplete, 1, 1);
+        }
 
     }
-
 }
