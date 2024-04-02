a0484fb345a81a0bb2a143790df7dbba2ec36d7c
HBrough-6 <130100349+HBrough-6@users.noreply.github.com>
2024-04-02 08:39:22 -0700

    Halfway UI Connection

--- GhostsGhoulsGoblins/GhostsGhoulsGoblins/Assets/Scripts/UI Scripts/UIManager.cs
+++ GhostsGhoulsGoblins/GhostsGhoulsGoblins/Assets/Scripts/UI Scripts/UIManager.cs
@@ -11,12 +11,33 @@ public class UIManager : MonoBehaviour
 
     // True when the player is in the MainMenus
     private bool notInGame;
+
+    // HUD element references
+    [SerializeField] private GameObject _HUDRef;
+    [SerializeField] private GameObject HealthBarRef;
+
+    // UI Element arrays
+    [SerializeField] private GameObject[] _HUDElements = new GameObject[5];
     // Start is called before the first frame update
     void Start()
     {
         currentMenuScreen = mainMenuScreen;
         Time.timeScale = 0;
+
+        _HUDRef = GameObject.FindGameObjectWithTag("HUD");
+
+        int index = 0;
+        GameObject currentChild;
+        // finds all of the children of the HUD and puts them in a list
+        while ((currentChild = _HUDRef.transform.GetChild(index).gameObject) != null)
+        {
+            _HUDElements[index] = currentChild;
+            currentChild = null;
+            index++;
+        }
     }
+
+    
 
     /// <summary>
     /// Returns the screen to the Main Menu Screen when the player has not started playing
@@ -52,4 +73,22 @@ public class UIManager : MonoBehaviour
     {
         Time.timeScale = 1;
     }
+
+    public void UpdateHealth(int newHealthTotal)
+    {
+        // update the health in the UI
+        
+
+    }
+
+    public void UpdateGold(int newGoldTotal)
+    {
+        // update the gold in the UI
+    }
+
+    public void UpdateDiveCharge()
+    {
+
+    }
+
 }
