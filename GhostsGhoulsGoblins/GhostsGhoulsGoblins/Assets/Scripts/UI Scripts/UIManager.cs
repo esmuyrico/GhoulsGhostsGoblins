fc1526aa6f4f9890f192944792e746c1ff051b4d
HBrough-6 <130100349+HBrough-6@users.noreply.github.com>
2024-04-02 09:39:22 -0700

    Added Singleton script

--- GhostsGhoulsGoblins/GhostsGhoulsGoblins/Assets/Scripts/UI Scripts/UIManager.cs
+++ GhostsGhoulsGoblins/GhostsGhoulsGoblins/Assets/Scripts/UI Scripts/UIManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class UIManager : MonoBehaviour
+public class UIManager : Singleton<UIManager>
 {
     [SerializeField]
     private GameObject mainMenuScreen;
