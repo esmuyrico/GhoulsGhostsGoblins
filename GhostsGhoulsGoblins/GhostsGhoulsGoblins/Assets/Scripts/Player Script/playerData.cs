d5c58b9e6ddcc22b9d5f6eacdd6f11a713f6f089
Gabriel Alter <gabriel_alter11@yahoo.com>
2024-04-11 00:39:40 -0700

    Added player health int

--- GhostsGhoulsGoblins/GhostsGhoulsGoblins/Assets/Scripts/Player Script/playerData.cs
+++ GhostsGhoulsGoblins/GhostsGhoulsGoblins/Assets/Scripts/Player Script/playerData.cs
@@ -5,23 +5,18 @@ using UnityEngine;
 public class PlayerData : MonoBehaviour
 {
     private int coinNum;
+    public int health = 10;
+
     // Start is called before the first frame update
     void Start()
     {
         coinNum = 0;
     }
 
-
     public void HurtPlayer()
     {
-
+        health--;
     }
-
-
-
-
-
-
 
     private void OnTriggerEnter(Collider other)
     {
@@ -35,21 +30,7 @@ public class PlayerData : MonoBehaviour
 
         if (other.gameObject.tag == "Enemy")
         {
-
             HurtPlayer();
-
-
-
-
-
-
-
         }
-
-
-
-
-
     }
-
 }
